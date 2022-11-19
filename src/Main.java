7e941e1307f899321a33a1ac403502e04d101611
Eva Pi <evao.costa@hotmail.com>
2022-11-19 15:38:35 +0100

    Estado inicial

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Estudiante "+args[0]+" : ");
+
+
+        String valor1=args[2];
+        String valor2="3";
+
+        System.out.println(veredicto.veredict(valor1, valor2));
+    }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/veredicto.java
@@ -0,0 +1,25 @@
+public class veredicto {
+
+    public  static String veredict(String valor1, String valor2){
+
+
+        if(valor1.equals(valor2)){
+            return "Bien. Pero una hora de estudio para cada modulo puede ser insuficiente";
+        }
+        if(Integer.valueOf(valor1)<Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes Debes dedicar más tiempo";
+
+        }
+        return "Ideal. Trabajas los contenidos en casa";
+
+    }
+
+
+
+    }
+
+
+
+
+
+
